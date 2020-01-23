2c97e08934e7762ec01357b4ea388d68fdd08a0a
Oliver <oliverdeng1005@hotmail.com>
2020-01-24 11:41:54 +1300

    update

---
+++ build/precache-manifest.715a0306fa0daf97c6ff23bdb3cb75c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e78463f2028c582350562aa3e39cd5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "abd0bc28bf86921184c1",
+    "url": "./static/css/3.5a1166ae.chunk.css"
+  },
+  {
+    "revision": "05b5bd11e2646686a300",
+    "url": "./static/css/main.d1eafc54.chunk.css"
+  },
+  {
+    "revision": "e8834847cae74005b646",
+    "url": "./static/js/2.2d04d101.chunk.js"
+  },
+  {
+    "revision": "2a045b45f45699734ba6ffbb145cee9c",
+    "url": "./static/js/2.2d04d101.chunk.js.LICENSE"
+  },
+  {
+    "revision": "abd0bc28bf86921184c1",
+    "url": "./static/js/3.566f3388.chunk.js"
+  },
+  {
+    "revision": "05b5bd11e2646686a300",
+    "url": "./static/js/main.bd95df6f.chunk.js"
+  },
+  {
+    "revision": "607a1ddbc4fae32216a0",
+    "url": "./static/js/runtime-main.0b953208.js"
+  },
+  {
+    "revision": "76f33ebc3c4dafb91c517126bfa72217",
+    "url": "./static/media/new_pearl_logo.76f33ebc.png"
+  }
+]);
