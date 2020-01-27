3114d854560bc376fd04397f8b479cd3a4cbb9b9
Oliver <oliverdeng1005@hotmail.com>
2020-01-28 11:51:19 +1300

    update

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+// app.listen(process.env.PORT||8080);
+//
+//
+
+
 const express = require('express');
 const path = require('path');
 const app = express();
+
+
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(process.env.PORT||8080);
+
+if (module === require.main) {
+
+    const server = app.listen(process.env.PORT || 8080, () => {
+        const port = server.address().port;
+        console.log(`App listening on port ${port}`);
+    });
+
+
+}
+
+
+module.exports = app;
